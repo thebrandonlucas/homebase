d427e0322b7d266dff00519cf9ba08b1e52ae4fd
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2020-04-11 13:05:11 -0400

    navbar change on scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
 
 function App() {
+  const [activeClass, setActiveClass] = useState('')
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      let navBackgroundClass = '';
+      if (window.scrollY > 100) {
+        navBackgroundClass = 'background-container';
+      }
+      setActiveClass(navBackgroundClass);
+    });
+  })
+
   return (
     <div id="bootstrap-overrides" className="App">
       <header className="App-header">
-        <div className="d-flex flex-column align-items-center justify-content-center min-vh-80">
-          <div className="background-container background-container-content">
-            <h1 className="title">Brandon Lucas</h1><br />
-            <h3 className="subtitle">Full-stack Web Development, Blockchain, Machine Learning</h3>
-          </div>
-        </div>
-        <div className="background-container sticky">
+        <div className={activeClass + " sticky-top"}>
           <Navbar expand="lg">
-            {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
             <Nav className="mr-auto">
-              <Nav.Link href="#aboutme">About Me</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
               <Nav.Link href="#projects">Projects</Nav.Link>
               <Nav.Link href="#experience">Experience</Nav.Link>
+              <Nav.Link href="#blog">Blog</Nav.Link>
             </Nav>
           </Navbar>
         </div>
+        <div className="d-flex flex-column align-items-center justify-content-center min-vh-title">
+          <div className="background-container-content">
+            <h1 className="title">Brandon Lucas</h1>
+            <h3 className="subtitle">Full-Stack Web Development, Blockchain, Machine Learning</h3>
+          </div>
+        </div>
+
+        <div className="min-vh-navbar"></div>
+
+        <div id="about" className="page-container fullscreen">
+          <h1>About me</h1>
+          <h1>About me</h1>
+          <h1>About me</h1>
+          <h1>About me</h1>
+
+          v<h1>About me</h1>
+          <h1>About me</h1>
+          <h1>About me</h1>
+        </div>
+        <div>
+          <h1>About me</h1>
+          <h1>About me</h1>
+          <h1>About me</h1>
+          <h1>About me</h1>
+
+          v<h1>About me</h1>
+          <h1>About me</h1>
+          <h1>About me</h1>
+        </div>
       </header>
+
     </div >
   );
 }
