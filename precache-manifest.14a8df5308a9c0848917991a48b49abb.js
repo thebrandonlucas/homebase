1d73e5313b4a6bc0afe0c6b71eb1671dec1c0d8f
Brandon Lucas <thebrandonlucas@gmail.com>
2022-05-20 12:37:48 -0400

    Updates

---
+++ precache-manifest.14a8df5308a9c0848917991a48b49abb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1363762b30b1686bb42a022429d42cc0",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "253662f042e765293a22",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bc1d6525061ca0ee7b9d",
+    "url": "/home/static/css/main.b418b601.chunk.css"
+  },
+  {
+    "revision": "253662f042e765293a22",
+    "url": "/home/static/js/2.e221cbfa.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/home/static/js/2.e221cbfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc1d6525061ca0ee7b9d",
+    "url": "/home/static/js/main.10e922e7.chunk.js"
+  },
+  {
+    "revision": "c32ce6cf244e1d25b7e4",
+    "url": "/home/static/js/runtime-main.5d4a792f.js"
+  },
+  {
+    "revision": "eaf3d6d0eef9409abbe0fe113573e0d7",
+    "url": "/home/static/media/BamaTheatre.eaf3d6d0.jpg"
+  },
+  {
+    "revision": "6e09e85487464ba4ff0bc9f8a248b7f5",
+    "url": "/home/static/media/IMG_2204.6e09e854.JPG"
+  },
+  {
+    "revision": "562abd211132cfc1febe71071674e5ec",
+    "url": "/home/static/media/IMG_2290.562abd21.JPG"
+  },
+  {
+    "revision": "222a63a4f1d336fdee66ddd1d73a0158",
+    "url": "/home/static/media/blucoin-mine.222a63a4.png"
+  },
+  {
+    "revision": "55a2a48524cbbcb42b825ae26c3ef97b",
+    "url": "/home/static/media/cfa_convo_dash.55a2a485.jpg"
+  },
+  {
+    "revision": "06b15dd82d08dcb44fce50684d7b1a0e",
+    "url": "/home/static/media/combat-deepfakes.06b15dd8.png"
+  },
+  {
+    "revision": "db9b2a42551426b1df94b1b44c4230f8",
+    "url": "/home/static/media/covid_cropped.db9b2a42.jpg"
+  },
+  {
+    "revision": "a256b837a7922e4cbcb861b90d737d69",
+    "url": "/home/static/media/scatterplot_mindcontrol.a256b837.png"
+  },
+  {
+    "revision": "213bcd5cae33df45d38bd4fd1699b9c7",
+    "url": "/home/static/media/tes-puppy.213bcd5c.png"
+  },
+  {
+    "revision": "bb1582d887448c437bca0b9d4a53e8c5",
+    "url": "/home/static/media/thebestme.bb1582d8.jpeg"
+  }
+]);
